341d1e60110043fcac2f3159a85d32bec20a743c
Jack Marshall <jack@localhost.localdomain>
2020-11-18 07:43:13 +1100

    first commit

---
+++ js/index.js
@@ -0,0 +1,62 @@
+
+//----------------------------------------------------------------//
+//IPHONE
+function iPhoneScreen() {
+    alert("iPhone Screen Replacement Selected")
+}
+
+function iPhoneBattery() {
+    alert("iPhone Battery Replacement Selected")
+}
+
+function iPhonePassword() {
+    alert("iPhone Password Removal Selected")
+}
+
+//----------------------------------------------------------------//
+//ANDROID
+
+function AndroidScreen() {
+    alert("iPhone Screen Replacement Selected")
+}
+
+function AndroidBattery() {
+    alert("iPhone Battery Replacement Selected")
+}
+
+function AndroidPassword() {
+    alert("iPhone Password Removal Selected")
+}
+
+//----------------------------------------------------------------//
+//COMPUTER
+
+function ComputerScreen() {
+    alert("Computer Screen Selected")
+}
+
+function ComputerBattery() {
+    alert("Computer Battery Selected")
+}
+
+function ComputerPassword() {
+    alert("Computer Password Selected")
+}
+
+function ComputerWinInstall() {
+    alert("Computer Windows Install Selected")
+}
+
+function ComputerHardUpgrade() {
+    alert("Computer Hardware Upgrade Selected")
+}
+
+function ComputerFileTransfer() {
+    alert("Computer File Transfer Selected")
+}
+
+//----------------------------------------------------------------//
+//MAC
+function mac() {
+
+}
